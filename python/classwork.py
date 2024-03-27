e9dfd0c0eb18a6f6540fed35a0c78a5c803adae9
mateberidze <160641356+mateberidze@users.noreply.github.com>
2024-03-27 20:46:02 +0400

    Delete python directory

--- python/classwork.py
+++
@@ -1,8 +0,0 @@
-number = float(input("please enter your number: "))
-
-if number > 0:
-    print("number is postive")
-elif number < 0:
-    print("number is not postive")
-else:
-    print("number is zero")
